5a6a84a908b6819582ac2fc1755f8432e9b3cc8d
Cláudio Sampaio (Patola) <patola@gmail.com>
2018-03-15 03:04:51 -0300

    Added just the header of the main (only?) file.

---
+++ desafiocit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+"""desafiocit: a simple microservices implementation for CI&T."""
+
+__author__ = "Cláudio 'Patola' Sampaio"
+__copyright__ = "Copyright 2018 by Cláudio Sampaio"
+__credits__ = ["Cláudio 'Patola' Sampaio", "CI&T"]
+__license__ = "MIT License"
+__version__ = "1.0"
+__maintainer__ = "Cláudio 'Patola' Sampaio"
+__email__ = "patola@gmail.com"
+__status__ = "Prototype"
+
+
